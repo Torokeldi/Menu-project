a0e5768831af35d76beaa88b4dc7d1a0b5b138fd
Tolik Toktakunov <tolik.toktakunov.kg@gmail.com>
2025-04-11 09:39:53 +0600

    adding two pages

--- src/app/pages/sixthPage/SixthPage.tsx
+++ src/app/pages/sixthPage/SixthPage.tsx
@@ -2,12 +2,30 @@ import Image from "next/image";
 import scss from "./SixthPage.module.scss";
 import Line from "../../../assets/images/Frame 10.png";
 import arrow from "../../../assets/images/arrow-right.svg";
-import phoneNumber from "../../../assets/images/SF Symbol2.png";
-import email from "../../../assets/images/SF Symbol.png";
-import telegram from "../../../assets/images/Telegram.png";
-import instagram from "../../../assets/images/Instagram.png";
+import phoneNumber from "../../../assets/images/SF Symbol.png";
+import email from "../../../assets/images/email.png";
+import telegram from "../../../assets/images/telegram.png";
+import instagram from "../../../assets/images/instagram.png";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import scss from "./SixthPage.module.scss";
+import { useEffect } from "react";
 
 const SixthPage = () => {
+  useEffect(() => {
+    const map = L.map("map", {
+      center: [34.1522, -118.1445], 
+      zoom: 15,
+    });
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([34.1522, -118.1445]).addTo(map).bindPopup("Наше местоположение");
+  }, []);
+
   return (
     <div className={scss.SixthPage}>
       <div className="container">
@@ -45,11 +63,10 @@ const SixthPage = () => {
               <Image src={instagram} alt="" />
             </div>
 
-            <img
-              className={scss.location}
-              src="https://s3-alpha-sig.figma.com/img/9446/9ed4/2722774a78cabe5da35fac0ede341734?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FHQi1Ig6CMWwLsvkjzI1CvW13WHq89LcAdYmBUhhWiHw3zf35txf0SvlNqeNK9UjmYnlxlFlRgiD-gAj5~va0elPkQe~erSsYm8i7THUDYf-ttyV8SfKiuBtG-bT8m6jLFkMBOH7ZORSBHIER6UW3umN2OPvanNcTq7V0jH~MpaRmRsvIg970zeQA3Z7su~cgc2c-xzjpnDUkmbaTMkt-9WWB-5Mdj6LGrqCQp56dTgcvFePsdpio2vf6i9c5xFKpexa0abKPwP8Nab7zJvgb4RYMMrL2ddfGV4toP2tad2j8aen52~5-qqeJ4vBoYMqxoFhRedFvZ5F9XKLrxyCKQ__"
-              alt=""
-            />
+            <div
+              id="map"
+              style={{ width: "100%", height: "400px", borderRadius: "1.7px" }}
+            ></div>
           </div>
         </div>
       </div>
